7ea57ac3c8f2fbb0e06391d3a0ed39b9e119454c
eadtlav <david.latortue@ericsson.com>
2019-10-10 20:04:24 -0400

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,103 @@
 <template>
-  <div class="content">
-    <b-navbar :class="{'dark-mode-navbar': isDark}">
-      <template slot="brand">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-         <img :src=" !isDark ? darkLogo : lightLogo" alt="Sonia logo" />
-        </b-navbar-item>
+  <div class="content columns is-gapless">
+    <div :class="{'hidden' : isHidden }" class="column is-one-fifth">
+        <template>
+          <b-menu>
+              <b-menu-list label="Menu">
+                  <b-menu-item icon="information-outline" label="Info"></b-menu-item>
+                  <b-menu-item
+                      icon="settings"
+                      :active="isActive"
+                      :expanded="isActive"
+                      @click="isActive = !isActive">
+                      <template slot="label" slot-scope="props">
+                          Administrator
+                          <b-icon
+                              class="is-pulled-right"
+                              :icon="props.expanded ? 'menu-down' : 'menu-up'">
+                          </b-icon>
+                      </template>
+                      <b-menu-item icon="account" label="Users"></b-menu-item>
+                      <b-menu-item icon="cellphone-link">
+                          <template slot="label" slot-scope="props">
+                              Devices
+                              <b-dropdown aria-role="list" class="is-pulled-right" position="is-bottom-left">
+                              <b-icon icon="dots-vertical" slot="trigger"></b-icon>
+                                  <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
+                                  <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
+                                  <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
+                              </b-dropdown>
+                          </template>
+                      </b-menu-item>
+                      <b-menu-item icon="cash-multiple" label="Payments" disabled></b-menu-item>
+                  </b-menu-item>
+                  <b-menu-item icon="account" label="My Account">
+                      <b-menu-item label="Account data"></b-menu-item>
+                      <b-menu-item label="Addresses"></b-menu-item>
+                  </b-menu-item>
+              </b-menu-list>
+              <b-menu-list>
+                  <b-menu-item
+                      label="Expo"
+                      icon="link"
+                      tag="router-link"
+                      target="_blank"
+                      to="/expo">
+                  </b-menu-item>
+              </b-menu-list>
+              <b-menu-list label="Actions">
+                  <b-menu-item label="Logout"></b-menu-item>
+              </b-menu-list>
+          </b-menu>
       </template>
-      <template slot="start">
-        <b-navbar-item :class="{'text-white': isDark}" tag="router-link" :to="{ name: 'home' }">Telemetry</b-navbar-item>
-        <b-navbar-item :class="{'text-white': isDark}" tag="router-link" :to="{ name: 'topics' }">Topics</b-navbar-item>
-        <b-navbar-dropdown :class="{'dark-mode-navbar': isDark}" label="Info">
-          <b-navbar-item tag="router-link" :to="{ name: 'about' }">About</b-navbar-item>
-          <b-navbar-item href="#">Contact (NOT DONE)</b-navbar-item>
-        </b-navbar-dropdown>
-      </template>
+    </div>
+    <div class="column"> 
+        <b-navbar :class="{'dark-mode-navbar': isDark}">   
+          <template slot="brand" >
+            <b-navbar-item :class="{'hover-dark': isDark,'hover-light': !isDark}"  tag="router-link" :to="{ path: '/' }">
+              <img @click="toggleSideBar" :src=" !isDark ? darkLogo : lightLogo" alt="Sonia logo" />
+            </b-navbar-item>
+          </template>
+          <template slot="start">
+            <b-navbar-item :class="{'text-white': isDark}" tag="router-link" :to="{ name: 'home' }">Telemetry</b-navbar-item>
+            <b-navbar-item :class="{'text-white': isDark}" tag="router-link" :to="{ name: 'topics' }">Topics</b-navbar-item>
+            <b-navbar-dropdown :class="{'dark-mode-navbar': isDark}" label="Info">
+              <b-navbar-item tag="router-link" :to="{ name: 'about' }">About</b-navbar-item>
+              <b-navbar-item href="#">Contact (NOT DONE)</b-navbar-item>
+            </b-navbar-dropdown>
+          </template>
 
-      <template slot="end">
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <a class="button is-dark" @click="toggleDarkMode">
-              <strong>Dark Mode</strong>
-            </a>
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light">Log in</a>
-          </div>
-        </b-navbar-item>
-      </template>
-    </b-navbar>
+          <template slot="end">
+            <b-navbar-item tag="div">
+              <div class="buttons">
+                <a class="button is-dark" @click="toggleDarkMode">
+                  <strong>Dark Mode</strong>
+                </a>
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">Log in</a>
+              </div>
+            </b-navbar-item>
+          </template>
+        </b-navbar>
 
-    <router-view></router-view>
-    
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    
+  },
   data: function() {
     return {
-      //isDark: false,
       lightLogo : require('./assets/logo_white.png'),
-      darkLogo : require('./assets/logo_color.png')
+      darkLogo : require('./assets/logo_color.png'),
+      isHidden : true,
+      isActive: true
     }
   },
   methods:{
@@ -49,6 +105,10 @@ export default {
       console.log("Dark mode is "+!this.isDark);
       //this.isDark = !this.isDark
       this.$store.commit("toggleDarkMode")
+    },
+    toggleSideBar: function(){
+      console.log("here")
+      this.isHidden = !this.isHidden
     }
   },
   computed:{
@@ -60,6 +120,15 @@ export default {
 </script>
 
 <style>
+.hover-dark:hover{
+  background-color: rgb(3, 16, 32)!important;
+}
+.hover-light:hover{
+  background-color: rgb(164, 216, 247)!important;
+}
+.hidden{
+  display: none!important;
+}
 .blue{
   background-color: #081B33!important;
 }
