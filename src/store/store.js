f8f2bbccbd4314b8d23f2fe84fc92498b40c902c
tortueTortue <dxlatortue@gmail.com>
2019-10-12 16:18:11 -0400

    Create component for sidebar

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         isDark: false,
-        isSidebarOpen: false
+        isSidebarOpen: false,
+        isSignedIn: false
     },
     mutations: {
         toggleDarkMode() {
@@ -14,6 +15,12 @@ export default new Vuex.Store({
         },
         toggleSideBar() {
             this.state.isSidebarOpen = !this.state.isSidebarOpen
+        },
+        signIn() {
+            this.state.isSignedIn = true
+        },
+        signOut() {
+            this.state.isSignedIn = false
         }
     },
     actions: {
