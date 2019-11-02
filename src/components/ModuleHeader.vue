46c2660b1e075d77e17a7d024b8b53bb37b031d6
Regan <quotidy@gmail.com>
2019-11-01 21:37:31 -0400

    create module-header and replace code

---
+++ src/components/ModuleHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav class="level module-nav">
+      <div class="level-left">
+        <span class="text-small unselectable">Index: {{ topic.i }}</span>
+      </div>
+      <div class="level-right">
+        <button
+          @click="fixModule(topic)"
+          class="button is-success is-small is-very-small is-outlined"
+        >
+          <b-icon size="is-small" icon="anchor"></b-icon>
+        </button>
+        <button
+          @click="minimizeModule(topic)"
+          class="button is-warning is-small is-very-small is-outlined"
+        >
+          <b-icon size="is-small" icon="window-minimize"></b-icon>
+        </button>
+        <button class="button is-danger is-small is-very-small is-outlined">
+          <b-icon size="is-small" icon="close"></b-icon>
+        </button>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["topic"],
+  methods: {
+    minimizeModule: function(topic) {
+      console.log("Minimize this module" + topic.i);
+      this.$store.commit("setModuleStatus", {
+        i: topic.i,
+        active: !topic.active
+      });
+    },
+    fixModule: function(topic) {
+      console.log("Fix this module" + topic.i);
+      topic.static = !topic.static;
+      topic.isDraggable = !topic.isDraggable;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
