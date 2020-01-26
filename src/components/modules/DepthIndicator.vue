6d0debada9cdb1165db39777a0f63c431ed644d1
Regan <quotidy.regan@gmail.com>
2020-01-26 17:11:02 -0500

    Add bulma dependency, add basic navigation without page routing

---
+++ src/components/modules/DepthIndicator.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="section padding-ver-null">
+      <div class="container">
+        <section>
+          <b-field
+            :class="{'text-white': isDark}"
+            v-for="t in thrusterEfforts"
+            :key="t.ID"
+            :label="'T'+t.ID"
+          >
+          </b-field>
+        </section>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "depth-indicator",
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {
+    depthIndicator() {
+      return this.$store.state.topicManager.depthIndicatorValue;
+    },
+    isDark() {
+      return this.$store.state.isDark;
+    }
+  }
+};
+</script>
+<style>
+</style>
